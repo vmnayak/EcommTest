6abd129f9c89fb06df94ee295df5791aa8c9d4df
vmnayak <vijayvmnayak@gmail.com>
2022-08-16 12:57:40 +0530

    run.bat

--- run.bat
+++ run.bat
@@ -1,2 +1,3 @@
-cmd /k "C:\SeleniumPython\nopCommerce\venv\Scripts\activate.bat" & "pytest -n=10 -m "sanity" --html=./Reports/MarkerReport.html testCases/ --browser chrome"
-pause
+call C:\SeleniumPython\nopCommerce\venv\Scripts\activate.bat
+call pytest -n=10 -m "sanity" --html=./Reports/MarkerReport.html testCases/ --browser chrome
+call pause
